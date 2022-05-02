65a932c45e07ba19d679d952e0382a50231e1f24
Alexjsja <alex.shemelev22@gmail.com>
2022-05-02 18:14:28 +0500

    fix compose up

--- compose_up.sh
+++ compose_up.sh
@@ -9,5 +9,5 @@ go build -o ./collection-service.bin ./backend/collection/cmd/main.go
 go build -o ./group-service.bin ./backend/group/cmd/main.go
 
 echo "run services detached"
-docker-compose build --force-rm --no-cache
-docker-compose up --detach
+docker-compose build --force-rm --no-cache auth collection
+docker-compose up --detach auth collection redis mongo
