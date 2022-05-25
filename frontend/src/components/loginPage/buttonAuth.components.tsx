99e65f0d84b07a886b1499597eb4bafb97bd910f
snaf-trip <egornesvickij@gmail.com>
2022-05-25 11:51:50 +0500

    Add types at button login

--- frontend/src/components/loginPage/buttonAuth.components.tsx
+++ frontend/src/components/loginPage/buttonAuth.components.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "./buttonAuth.components.scss";
 
-export const ButtonAuth = ({ onClick, textContent }: any) => {
-  return <a id="loginBtn" onClick={onClick} className="form__button">{textContent}</a>;
+export const ButtonAuth = (
+  { onClick, textContent }:
+    { onClick: React.MouseEventHandler<HTMLAnchorElement>, textContent: string }) => {
+  return (
+    <a
+      id="loginBtn"
+      onClick={onClick}
+      className="form__button">
+      {textContent}
+    </a>
+  );
 };
