5dc6b6d4e59792d0a73cdfc0d27b3f3e8d3559f4
snaf-trip <egornesvickij@gmail.com>
2022-05-29 16:49:34 +0500

    Refactored menu.components

--- frontend/src/components/menu/menu.componenst.tsx
+++ frontend/src/components/menu/menu.componenst.tsx
@@ -1,15 +1,26 @@
-import React from "react";
 import './menu.componenst.scss';
 import { NavLink } from 'react-router-dom';
 import { MenuItem } from "./menuItems/menuItem.components";
 
-export const Menu = () => {
+export const Menu = (): JSX.Element => {
   return (
     <div className="menuContainer">
-      <NavLink className={({ isActive }) => isActive ? 'menuContainer__link item_active' : 'menuContainer__link'} to='/profile'>
+      <NavLink
+        className={
+          ({ isActive }) =>
+            isActive ? 'menuContainer__link item_active' : 'menuContainer__link'
+        }
+        to='/profile'
+      >
         <MenuItem name={'Profile'} active={false} />
       </NavLink>
-      <NavLink className={({ isActive }) => isActive ? 'menuContainer__link item_active' : 'menuContainer__link'} to='/home'>
+      <NavLink
+        className={
+          ({ isActive }) =>
+            isActive ? 'menuContainer__link item_active' : 'menuContainer__link'
+        }
+        to='/home'
+      >
         <MenuItem name={'Home'} active={true} />
       </NavLink>
       <MenuItem name={'My Dictionary'} active={false} />
