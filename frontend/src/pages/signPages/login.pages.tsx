ddbfb5917d383e8ad34159f978245825b74c54de
snaf-trip <egornesvickij@gmail.com>
2022-05-29 16:28:56 +0500

    Mini changes code style

--- frontend/src/pages/signPages/login.pages.tsx
+++ frontend/src/pages/signPages/login.pages.tsx
@@ -2,11 +2,9 @@ import { Navigate } from 'react-router-dom';
 import { SignForm } from '../../components/signPages/form.components';
 
 export const LoginPage = (): JSX.Element | null => {
-
   if (localStorage.getItem('token') === null) {
     return <SignForm page={'logIn'} title={'Welcome to EngLearn'} buttonContent={'Log in'} />
   } else {
-    return <Navigate to={'/'}></Navigate>
+    return <Navigate to={'/'} />
   }
-
 };
