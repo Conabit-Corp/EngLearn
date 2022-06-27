1decbd3663786a493f0acf5a412ab538b99436c1
navzlet <sasha_zakirov_2014@mail.ru>
2022-06-27 16:32:58 +0500

    added request to get collections

--- frontend/src/apiGRPC/collectionService.ts
+++ frontend/src/apiGRPC/collectionService.ts
@@ -1,9 +1,22 @@
 import { grpc } from "@improbable-eng/grpc-web";
 import { WordCollectionService } from "../../proto/conabit/englearn/collection/collection_service_pb_service";
-import { CreateWordCollectionRequest } from "../../proto/conabit/englearn/collection/collection_transport_pb";
+import { CreateWordCollectionRequest, GetUserCollectionsRequest } from "../../proto/conabit/englearn/collection/collection_transport_pb";
 import { Session } from "../../proto/conabit/englearn/common/session_pb";
 import { newWordCollection } from "../utils/export.utils";
 import { WordObj } from "../pages/createCollection/createCollection.pages";
+
+interface CollectionOverviewsResponse {
+  overview: CollectionsOverwies
+}
+
+interface CollectionsOverwies {
+  collectionsList: Array<CollectionOverview>
+}
+
+interface CollectionOverview {
+  collectionId: string
+  collectionName: string
+}
 
 export const createCollectionRequest = (collectionTitle: string, collectionDescription: string, words: Array<WordObj>) => {
   const session = new Session()
@@ -20,5 +33,23 @@ export const createCollectionRequest = (collectionTitle: string, collectionDescr
       }
     }
   )
+}
+
+export const getWordsCollections = (setCollections: React.Dispatch<React.SetStateAction<Object>>) => {
+  const req = new GetUserCollectionsRequest()
+  console.log(req.toObject());
+
+  const session = new Session()
+  session.setJwt(localStorage.getItem('token') ?? '')
+  req.setSession(session)
+  grpc.unary(WordCollectionService.GetUserWordCollections,
+      {
+          request: req,
+          host: "http://10.3.21.205:4003",
+          onEnd: (r) => {
+              const response = r.message?.toObject() as CollectionOverviewsResponse
+              setCollections(response)
+          }
+      })
 }
 
--- frontend/src/pages/collectionsPage/collectionsPage.pages.tsx
+++ frontend/src/pages/collectionsPage/collectionsPage.pages.tsx
@@ -1,7 +1,16 @@
 import { ColletionItem } from '../../components/collections/collectionItem.components'
+import {getWordsCollections} from '../../apiGRPC/collectionService'
+import { useState, useEffect } from 'react'
 import './collectionsPage.pages.scss'
 
 export const CollectionsPage = (): JSX.Element => {
+  const [collections, setCollections] = useState({});
+  
+  useEffect(()=>{
+    getWordsCollections(setCollections);
+  },[])
+  
+  console.log(collections)
   return (
     <div className="collectionsContainer">
      <div className='collectionsList'>
