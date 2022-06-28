9dc43952e2081cb9e328c562ce8d59cdf1d2acf2
snaf-trip <egornesvickij@gmail.com>
2022-06-28 17:10:00 +0500

    Updated icons size.

--- frontend/src/images/menu/collectionsIcon.tsx
+++ frontend/src/images/menu/collectionsIcon.tsx
@@ -3,8 +3,8 @@ import { SVGProps } from "react"
 
 export const CollectionsIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width={23}
-    height={23}
+    width={20}
+    height={20}
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
@@ -12,8 +12,8 @@ export const CollectionsIcon = (props: SVGProps<SVGSVGElement>) => (
     <rect
       x={1}
       y={1}
-      width={21}
-      height={21}
+      width={18}
+      height={18}
       rx={4}
       stroke="#ACACAC"
       strokeWidth={2}
@@ -21,7 +21,7 @@ export const CollectionsIcon = (props: SVGProps<SVGSVGElement>) => (
     <path
       stroke="#ACACAC"
       strokeWidth={2}
-      d="M12.04.92v21.16M12.88 13.88H.92M23 8.36H11.04"
+      d="m10.565 1.737.035 17.461M9.651 12.304H1.738M18.265 7.165h-6.961"
     />
   </svg>
 )
--- frontend/src/images/menu/profileIcon.tsx
+++ frontend/src/images/menu/profileIcon.tsx
@@ -3,15 +3,19 @@ import { SVGProps } from "react"
 
 export const ProfileIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width={19}
-    height={25}
+    width={20}
+    height={20}
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path
-      d="M18.875 25h-2.083v-5.253a3.084 3.084 0 0 0-3.08-3.08H5.288a3.083 3.083 0 0 0-3.08 3.08V25H.124v-5.253a5.17 5.17 0 0 1 5.164-5.164h8.422a5.17 5.17 0 0 1 5.164 5.164V25ZM9.5 12.5A6.25 6.25 0 1 1 9.5 0a6.25 6.25 0 0 1 0 12.5Zm0-10.417a4.167 4.167 0 1 0 0 8.334 4.167 4.167 0 0 0 0-8.334Z"
-      fill="#ACACAC"
-    />
+    <g clipPath="url(#a)" fill="#ACACAC">
+      <path d="M17.5 20h-1.667v-4.202a2.467 2.467 0 0 0-2.464-2.465H6.631a2.467 2.467 0 0 0-2.464 2.465V20H2.5v-4.202a4.136 4.136 0 0 1 4.13-4.131h6.74a4.136 4.136 0 0 1 4.13 4.13V20ZM10 10a5 5 0 1 1 0-10 5 5 0 0 1 0 10Zm0-8.333a3.333 3.333 0 1 0 0 6.666 3.333 3.333 0 0 0 0-6.666Z" />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M0 0h20v20H0z" />
+      </clipPath>
+    </defs>
   </svg>
 )
