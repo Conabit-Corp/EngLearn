5a44715aa9820cad17da0423e86bc863712fe487
snaf-trip <egornesvickij@gmail.com>
2022-07-03 17:46:46 +0500

    Added go to collections button.

--- frontend/src/pages/collectionPage/collection.pages.tsx
+++ frontend/src/pages/collectionPage/collection.pages.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import "./collection.pages.scss";
 import { WordCollection } from "../../../proto/conabit/englearn/collection/collection_models_pb";
 import { getCollectionByIdRequest } from "../../apiGRPC/collectionService";
+import { ArrowLeftIcon } from "../../images/collections/arrowLeftIcon";
 
 export const Collection = () => {
   const [collection, setCollection] = useState<WordCollection.AsObject>();
@@ -18,7 +20,13 @@ export const Collection = () => {
 
 
   return (
-    <div>
+    <div className="collectionContainer">
+      <div className="collectionContainer__goBack">
+        <Link to={"/collections"}>
+          <ArrowLeftIcon className="collectionContainer__goBackIcon" />
+          <span className="collectionContainer__goBackText">Collections</span>
+        </Link>
+      </div>
       Collection id: {id}
     </div>
   )
