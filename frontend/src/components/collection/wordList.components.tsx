4a94591287090f9ffeb92c813357532be43d2e81
snaf-trip <egornesvickij@gmail.com>
2022-07-03 23:58:24 +0500

    Added render words.

--- frontend/src/components/collection/wordList.components.tsx
+++ frontend/src/components/collection/wordList.components.tsx
@@ -1,20 +1,31 @@
-import { WordCollection } from "../../../proto/conabit/englearn/collection/collection_models_pb";
 import "./wordList.components.scss";
+import { WordCollection } from "../../../proto/conabit/englearn/collection/collection_models_pb";
+import { WordItem } from "./wordItem.components";
 
 interface Props {
   collection: WordCollection.AsObject | undefined,
 }
 
 export const WordList = (props: Props): JSX.Element => {
+  console.log(props.collection?.wordsList);
   return (
-    <div className={`wordList  ${props.collection?.wordsList.length === 0 ? "wordList_noWords" : null}`}>
+    <div className={`wordList  ${props.collection?.wordsList.length === 0 ? "wordList_noWords" : ""}`}>
       {props.collection?.wordsList.length === 0 ?
         <p className="wordList__noWordsWarning">
           You haven’t added the words yet.<br />
           Add at least <strong>5 words</strong> to start exercises
         </p>
         :
-        <span className="wordList__wordsCount">words: {props.collection?.wordsList.length}</span>
+        <>
+          <span className="wordList__wordsCount">words: {props.collection?.wordsList.length}</span>
+          {
+            props.collection?.wordsList.map(word => {
+              return (
+                <WordItem key={word.id} word={word} />
+              )
+            })
+          }
+        </>
       }
     </div>
   )
