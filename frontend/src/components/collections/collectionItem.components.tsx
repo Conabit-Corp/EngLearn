1947a9c7255f7f174b7489acce0c37017fa50de8
navzlet <sasha_zakirov_2014@mail.ru>
2022-07-04 19:40:54 +0500

    added click away listener

--- frontend/src/components/collections/collectionItem.components.tsx
+++ frontend/src/components/collections/collectionItem.components.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./collectionItem.components.scss";
 import { CollectionPopupMenuIcon } from "../../images/collections/collectionPopupMenu";
 import { CollectionPopper } from "./collectionPopper.components";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
 
 interface Props {
   key: string;
@@ -12,27 +13,34 @@ interface Props {
 
 export const CollectionItem = (props: Props): JSX.Element => {
   const [isPoperOpen, setIsPoperOpen] = useState(false);
+
+  const handleClickAway = () => {
+    setIsPoperOpen(false);
+  };
+
   return (
-    <div className="collection">
-      <div className="collection__info">
-        <div className="collection__title">{props.collectionName}</div>
-        <div className="collection__description">
-          {props.collectionDescription}
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <div className="collection">
+        <div className="collection__info">
+          <div className="collection__title">{props.collectionName}</div>
+          <div className="collection__description">
+            {props.collectionDescription}
+          </div>
+        </div>
+        <div
+          onClick={() =>
+            isPoperOpen ? setIsPoperOpen(false) : setIsPoperOpen(true)
+          }
+          className="collection__popupButton"
+        >
+          {isPoperOpen ? (
+            <CollectionPopupMenuIcon className="collection__popupIcon_active" />
+          ) : (
+            <CollectionPopupMenuIcon className="collection__popupIcon" />
+          )}
+          {isPoperOpen ? <CollectionPopper /> : null}
         </div>
       </div>
-      <div
-        onClick={() =>
-          isPoperOpen ? setIsPoperOpen(false) : setIsPoperOpen(true)
-        }
-        className="collection__popupButton"
-      >
-        {isPoperOpen ? (
-          <CollectionPopupMenuIcon className="collection__popupIcon_active" />
-        ) : (
-          <CollectionPopupMenuIcon className="collection__popupIcon" />
-        )}
-        {isPoperOpen ? <CollectionPopper /> : null}
-      </div>
-    </div>
+    </ClickAwayListener>
   );
 };
